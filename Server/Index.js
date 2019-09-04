8133347b1cba3bd249f7ce3992e6d859cef3cc5f
Wendy Wu <Wendy@Wendys-MacBook-Air.local>
2019-09-04 16:04:36 -0700

    Add .css file and edit html file

--- Server/Index.js
+++ Server/Index.js
@@ -1,12 +1,22 @@
 const express = require('express'); 
+const path = require('path'); 
 
 const app = express();
-const port = 4000; 
+const port = 3005; 
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+app.get('/bundle.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/bundle.js'), (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('retreiving bundle files');
+        }
+    })
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
